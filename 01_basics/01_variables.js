8b1f9ffba0d5c3e1ba2e41d8c4627a0ea31365cf
kashishp26 <102727217+kashishp26@users.noreply.github.com>
2024-04-09 12:14:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=143
+let accountEmail ="chinuparashar6@gmail.com"
+// var accountPassword ="1223" prefer not to use var because of issue in block scope and functional scope
+accountCity="Indore"
+let accountState;
+
+// accountId=2 // not allowed
+
+accountEmail="benu@google.com"
+accountPassword="1234"
+accountCity="surat"
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs,not browser
+let name="kashu"
+let age=18
+let islogin=false
+console.log("kashu");
+
+// no => 2 to power 53
+// BigInt
+// string=> ""
+// boolean => true/false
+// null=>standalone value
+// undefined =>abhi value assign nhi hui h
+//symbol =>unique
+
+
+//object
+
+console.log( typeof "kashu")
+console.log(typeof age);
+console.log(typeof null); //object output aayega interview Q
+console.log(typeof undefined); //undefined hi o/p aayega
+
